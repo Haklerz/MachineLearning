5235d9e81ba794006547f0a23da8c4e0904a2b71
Håkon Lervik <lervik.haakon@gmail.com>
2020-10-14 22:12:14 +0200

    Messed some more with stuff

--- src/com/haklerz/ml/TensorTest.java
+++ src/com/haklerz/ml/TensorTest.java
@@ -2,7 +2,19 @@ package com.haklerz.ml;
 
 public class TensorTest {
     public static void main(String[] args) {
-        Tensor X = new Tensor(4, 3);
+        Tensor X = Tensor.normal(3, 3);
+
+        for (int col = 0; col < 3; col++) {
+            for (int row = 0; row < 3; row++)
+                System.out.print(String.format("%10.6f ", X.get(col, row)));
+            System.out.println();
+        }
+
+        Model model = new Model(64 * 64)
+            .addDense(1000, Activation.RELU)
+            .addDense(400, Activation.RELU)
+            .addDense(1000, Activation.RELU)
+            .addDense(64 * 64, Activation.SIGMOID);
 
     }
 }
