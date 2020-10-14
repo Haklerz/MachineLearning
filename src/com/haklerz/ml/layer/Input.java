a404e1e67ddc46d46c225bbb2e6e4969006745fb
Håkon Lervik <lervik.haakon@gmail.com>
2020-10-14 22:12:35 +0200

    Created Input layer

---
+++ src/com/haklerz/ml/layer/Input.java
@@ -0,0 +1,16 @@
+package com.haklerz.ml.layer;
+
+import com.haklerz.ml.Tensor;
+
+public class Input extends Layer {
+
+    public Input(int inputSize) {
+        super(inputSize);
+    }
+
+    @Override
+    public Tensor forward(Tensor input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
