0197f9a2e9a50856bf7800f2a84f3221daa6644a
Håkon Lervik <lervik.haakon@gmail.com>
2020-10-14 22:11:58 +0200

    Added transposition and some other stuff to Tensor

--- src/com/haklerz/ml/Tensor.java
+++ src/com/haklerz/ml/Tensor.java
@@ -1,42 +1,44 @@
 package com.haklerz.ml;
 
+import java.util.Random;
+
 /**
  * A tensor
  */
 public class Tensor {
-    private int[] shape;
     private double[] values;
+    private int[] shape;
+    private int[] indexMap;
 
     /**
      * Creates a tensor of a given shape and with given values.
      * 
      * @param shape The shape
      */
-    public Tensor(int[] shape, double[] values) {
-        this.shape = shape;
+    public Tensor(double[] values, int... shape) {
+        if (values.length != size(shape))
+            throw new IllegalArgumentException();
+
         this.values = values;
+        this.shape = shape;
+        this.indexMap = new int[shape.length];
+        for (int i = 0; i < indexMap.length; i++)
+            indexMap[i] = i;
     }
 
     /**
-     * Creates a tensor of a given shape.
+     * Creates a tensor of a given shape with normally distributed values.
      * 
      * @param shape
+     * @return
      */
-    public Tensor(int... shape) {
-        this(shape, new double[size(shape)]);
-    }
+    public static Tensor normal(int... shape) {
+        Random random = new Random();
+        double[] values = new double[size(shape)];
+        for (int i = 0; i < values.length; i++)
+            values[i] = 0.01 * random.nextGaussian();
 
-    public static Tensor rand(int... shape) {
-        return new Tensor(shape);
-    }
-
-    /**
-     * Returns the shape of the tensor.
-     * 
-     * @return The shape of the tensor
-     */
-    public int[] shape() {
-        return shape;
+        return new Tensor(values, shape);
     }
 
     /**
@@ -47,6 +49,55 @@ public class Tensor {
      */
     public double get(int... indices) {
         return values[rawIndex(indices)];
+    }
+
+    /**
+     * Set a value of the tensor at a given set of indices.
+     * 
+     * @param value
+     * @param indices
+     */
+    public void set(double value, int... indices) {
+        values[rawIndex(indices)] = value;
+    }
+
+    /**
+     * Returns the shape of the tensor.
+     * 
+     * @return The shape of the tensor
+     */
+    public int[] shape() {
+        return shape.clone();
+    }
+
+    /**
+     * Reshapes the tensor to a new shape.
+     * 
+     * @param shape The new shape
+     */
+    public Tensor reshape(int... shape) {
+        if (values.length != size(shape))
+            throw new IllegalArgumentException();
+
+        this.shape = shape;
+        return this;
+    }
+
+    /**
+     * Flattens the tensor.
+     * 
+     * @return
+     */
+    public Tensor flatten() {
+        reshape(size(shape));
+        return this;
+    }
+
+    public Tensor transpose(int axis0, int axis1) {
+        int temp = indexMap[axis0];
+        indexMap[axis0] = indexMap[axis1];
+        indexMap[axis1] = temp;
+        return this;
     }
 
     /**
@@ -69,24 +120,24 @@ public class Tensor {
      * @return
      */
     private int rawIndex(int... indices) {
-        int index = 0;
+        if (indices.length != shape.length)
+            throw new IllegalArgumentException();
+
+        int rawIndex = 0;
         int stride = 1;
         for (int i = 0; i < indices.length; i++) {
-            index += indices[i] * stride;
-            stride *= shape[i];
-        }
-        return index;
-    }
+            int index = indices[indexMap[i]];
+            int size = shape[indexMap[i]];
 
-    /**
-     * Reshapes the tensor to a new shape.
-     * 
-     * @param shape The new shape
-     */
-    public void reshape(int... shape) {
-        if (values.length != size(shape)) {
-            throw new IllegalArgumentException();
+            if (index < 0)
+                index += size;
+
+            if (index < 0 || index >= size)
+                throw new IndexOutOfBoundsException();
+
+            rawIndex += index * stride;
+            stride *= size;
         }
-        this.shape = shape;
+        return rawIndex;
     }
 }
